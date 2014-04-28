35f69fed6842ab8a38df3921de6adbeff708fd08
CathyGerald <jessicaaurelia@yahoo.fr>
2014-04-28 11:46:27 -0700

    Corporate summary
    route created

---
+++ src/Earls/CorporateBundle/Controller/CorporateSummaryController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Earls\CorporateBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CorporateSummaryController extends Controller{
+
+	/**
+     * @Route("/", name="_corporateSummary")
+     * @Template()
+     */
+    public function indexAction()
+    {
+
+        return $this->render('EarlsCorporateBundle:CorporateSummary:index.html.twig');
+
+    }
+
+
+
+}
+
+?>
